e6bda0a222d6cd317309bbad2a4c422bc0103fcb
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-03-26 23:13:15 +0600

    site complete

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,33 +4,38 @@ import SingleProduct from "../SingleProduct/SingleProduct";
 import "./Shop.css";
 
 const Shop = () => {
+  // ADD PRODUCT STATE
   const [products, setProducts] = useState([]);
+  // ADD CART PRODUCT STATE
   const [addCart, setAddCart] = useState([]);
+  // ADD RANDOM SHOW ITEM STATE
   const [random, setRandom] = useState([]);
+  // JSON FECTH DATA LODADED
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  // HANDLE ADD TO CART FUNCTION ADDED
   const handleAddToCart = (props) => {
     const newCart = [...addCart, props];
     setAddCart(newCart);
   };
-
+  // GENARET RENDOM PPRODUCT SHOW FUNCTIOIN
   const randomItem = () => {
     const random = addCart[Math.floor(Math.random() * addCart.length)];
     setRandom(random);
   };
-
+  // CLEAR ALL DATA FUNCTION ADDED
   const handleClearCart = () => setAddCart([]) || setRandom([]);
   return (
     <div className="shop-container">
       <div className="products-container">
-        {products.map((product) => (
+        {products.map((product, idx) => (
           <SingleProduct
             addCart={handleAddToCart}
             product={product}
-            key={product.id}
+            key={idx}
           ></SingleProduct>
         ))}
       </div>
@@ -38,16 +43,16 @@ const Shop = () => {
         <div className="carts-container">
           <div className="sticky-items">
             <h2 className="cart-title">Selected Items</h2>
-            {addCart.map((product) => (
-              <Cart product={product} key={product.id}></Cart>
+            {addCart.map((product, idx) => (
+              <Cart product={product} key={idx}></Cart>
             ))}
             {random === undefined ? (
               []
             ) : (
-              <div>
+              <div className="random-container">
                 <h3 className="random-item-title">Use Defult Random Item</h3>
                 <div className="random-item-info">
-                  <img src={random.img} alt="" />
+                  <img id="hideImg" src={random?.img} alt="" />
                   <p>{random?.name}</p>
                 </div>
               </div>
